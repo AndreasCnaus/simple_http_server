30e936d00761b02ba1986edc1da32c86a93a5670
AndreasCnaus <cnaus.andreas@gmail.com>
2025-11-19 10:18:05 +0100

    [Plot] filter for date and time added

--- plot_gps.py
+++ plot_gps.py
@@ -1,6 +1,8 @@
 import plotly.express as px
+from datetime import datetime
 import pandas as pd
 import sqlite3
+
 
 def main():
     # Connect to the SQLite Database
@@ -9,6 +11,8 @@ def main():
     # Read the entire table into a DataFrame
     df = pd.read_sql_query("SELECT * FROM gps_data", conn)
     conn.close()
+    
+    print(df)
 
     # Convert Date-Time into timestamp
     df["timestamp"] = pd.to_datetime({
@@ -19,10 +23,30 @@ def main():
         "minute": df["minute"],
         "second": df["second"]
     })
-
+    
+    # Date, start and end times for the filter
+    target_date_str = "2025-11-18"
+    start_time_str = "12:13:51"
+    end_time_str = "12:23:28"
+    
+    # Prepare filtering values
+    target_date = pd.to_datetime(target_date_str).date()
+    start_time = datetime.strptime(start_time_str, "%H:%M:%S").time()
+    end_time = datetime.strptime(end_time_str, "%H:%M:%S").time()
+    
+    # Create the Date Mask
+    mask_date = df["timestamp"].dt.date == target_date
+    
+    # Create Time Mask
+    time_only = df["timestamp"].dt.time
+    mask_time = (time_only >= start_time) & (time_only <= end_time)
+    
+    # Apply combined filter
+    df_filtered = df.loc[mask_date & mask_time].copy()
+    
     # Plot lines + points in one go
     fig = px.scatter_mapbox(
-        df,
+        df_filtered,
         lat="latitude",
         lon="longitude",
         hover_name="timestamp",
@@ -35,14 +59,14 @@ def main():
     fig.update_traces(mode="lines+markers",
                       marker=dict(size=8, color="red", opacity=0.9),
                       line=dict(color="blue", width=2),
-                      hovertemplate=df.apply(
+                      hovertemplate=df_filtered.apply(
                           lambda row: (
                               f"Lat: {row['latitude']:.5f}<br>"
                               f"Lon: {row['longitude']:.5f}<br>"
                               f"Date: {row['timestamp'].strftime('%d.%m.%Y')}<br>"
                               f"Time: {row['timestamp'].strftime('%H:%M:%S')}<br>"
                               f"Altitude: {row['altitude']} m<br>"
-                              f"Velocity: {row['speed']} km/h"
+                              f"Velocity: {row['speed']/100} km/h"
                           ), axis=1
                       )
     )
@@ -50,7 +74,7 @@ def main():
     # Use free OpenStreetMap tiles
     fig.update_layout(
         mapbox_style="open-street-map",
-        mapbox_center={"lat": df["latitude"].mean(), "lon": df["longitude"].mean()},
+        mapbox_center={"lat": df_filtered["latitude"].mean(), "lon": df_filtered["longitude"].mean()},
         margin={"r": 0, "t": 40, "l": 0, "b": 0}
     )
 
